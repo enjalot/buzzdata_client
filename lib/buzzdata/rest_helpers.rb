f9b20773d85ed663a4397fb88583bc3ddd10b26f
Robin Ward <robin@buzzdata.com>
2011-09-19 16:45:47 -0400

    DRYed up rest_helpers

--- lib/buzzdata/rest_helpers.rb
+++ lib/buzzdata/rest_helpers.rb
@@ -6,58 +6,32 @@ module RestHelpers
     raise Buzzdata::Error.new(parsed_error['message'])
   end
 
-  def post(url, params={})
-    params['api_key'] = @api_key
-    RestClient.post(url, params) do |response, request, result, &block|
-      case response.code
-      when 403, 404, 500
-        handle_error(response)
-      else
-        response.return!(request, result, &block)
-      end
-    end
-  end
+  # Define methods for our HTTP verbs
+  [:post, :put, :get, :delete].each do |method|
     
-  def get(url, params={})
-    params['api_key'] = @api_key
-    RestClient.get(url, :params => params) do |response, request, result, &block|
-      case response.code
-      when 403, 404, 500
-        handle_error(response)
-      else
-        response.return!(request, result, &block)
+    define_method(method) do |url, params={}|
+      params['api_key'] = @api_key
+
+      RestClient.send(method, url, params) do |response, request, result, &block|        
+        case response.code
+        when 403, 404, 500
+          handle_error(response)
+        else
+          response.return!(request, result, &block)
+        end
       end
     end
-  end
 
-  def delete(url, params={})
-    params['api_key'] = @api_key
-    RestClient.delete(url, :params => params) do |response, request, result, &block|
-      case response.code
-      when 403, 404, 500
-        handle_error(response)
-      else
-        response.return!(request, result, &block)
-      end
+    # Define methods for our verbs with json handling
+    define_method("#{method}_json") do |path, params={}|
+      response = send(method, path, params)
+      JSON.parse(response.body)
     end
-  end
 
-  def get_json(path, params={})
-    response = get(path, params)    
-    JSON.parse(response.body)
-  end
-
-  def post_json(path, params={})
-    response = post(path, params)    
-    json = JSON.parse(response.body)
-  end
-
-  def delete_json(path, params={})
-    response = delete(path, params)    
-    json = JSON.parse(response.body)
   end
   
   def raw_get(url)
     RestClient.get(url)
   end
+
 end
